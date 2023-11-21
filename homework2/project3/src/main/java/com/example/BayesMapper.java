3d98804f65f49faf1dbc4179abf8891880d567fa
Lsc <3469182707@qq.com>
2023-11-21 18:13:28 +0800

    hh

--- homework2/project3/src/main/java/com/example/BayesMapper.java
+++ homework2/project3/src/main/java/com/example/BayesMapper.java
@@ -1,50 +1,55 @@
 package com.example;
-import java.io.IOException;   
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;  
-import org.apache.hadoop.mapreduce.Mapper;  
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.hadoop.mapreduce.Mapper;
  
-public class BayesMapper extends Mapper<Object, Text, IntWritable, MyWritable> {  
-    Logger log = LoggerFactory.getLogger(BayesMapper.class);  
+public class BayesMapper extends Mapper<Object, Text, IntWritable, MyWritable> {
     private IntWritable myKey = new IntWritable();  
     private MyWritable myValue = new MyWritable();
     @Override  
     protected void map(Object key, Text value, Context context)  
-            throws IOException, InterruptedException {  
-        log.info("***"+value.toString());  
-        int[] values = getIntData(value);  
-        int label = values[0];  //存放类别  
-        int[] result = new int[values.length-1]; //存放数据  
-        for(int i =1;i<values.length;i++){  
-            result[i-1] = values[i];
-        }  
-        myKey.set(label);  
-        myValue.setValue(result);  
-        context.write(myKey, myValue);  
+            throws IOException, InterruptedException {
+        int[] values = getIntData(value); 
+        if(!(values[0]==-1)){
+            int label = values[0];  //存放类别  
+            int[] result = new int[values.length-1]; //存放数据  
+            for(int i =1;i<values.length;i++){  
+                result[i-1] = values[i];
+            }  
+            myKey.set(label);  
+            myValue.setValue(result);  
+            context.write(myKey, myValue);
+        }
     }  
     private int[] getIntData(Text value) {  
         String[] values = value.toString().split(",");  
-        int[] data = new int[42];
-        if (values.length == 56) {
-            if (!values[2].equals("")) {
-                // 如果第3列为M，则为1；如果为F，则为0
-                data[0] = values[2].equalsIgnoreCase("M") ? 1 : 0;
-            }
-            if (!values[3].equals("")) {
-                // 如果第4列为Y，则为1；如果为N，则为0
-                data[1] = values[3].equalsIgnoreCase("Y") ? 1 : 0;
-            }
-            int j=1;
-            for(int i=3; i < values.length;i++){
-                if(!values[i].equals("")&&values[i].matches("^[0-9]+$")){
-                    j++;
-                    data[j] = Integer.parseInt(values[i]);
-                }  
+        int[] data = new int[34];
+        Arrays.fill(data, -1); // 初始化数据数组为-1，表示数据无效
+        boolean skipRow = false; // 用于标记是否跳过当前行
+    
+        for (int i = 0; i < 34; i++) {
+            if (values[i].isEmpty()) {
+                skipRow = true;
+                break;  // 如果发现空值，将标记为跳过当前行，并终止循环
             }
         }
-        
+        if (!skipRow) {
+            // 处理非空数据行的代码
+            data[0] = Integer.parseInt(values[0]);
+            if (!values[1].isEmpty() && !values[2].isEmpty() && !values[3].isEmpty()) {
+                // 如果为M，则为1；如果为F，则为0
+                data[1] = values[1].equalsIgnoreCase("M") ? 1 : 0;
+                // 如果为Y，则为1；如果为N，则为0
+                data[2] = values[2].equalsIgnoreCase("Y") ? 1 : 0;
+                data[3] = values[3].equalsIgnoreCase("Y") ? 1 : 0;
+            }
+            for (int i = 4; i < 34; i++) {
+                data[i] = Integer.parseInt(values[i]);  
+            }
+        }
         return data;  
-    }
+    }    
 }  
--- homework2/project3/src/main/java/com/example/BayesReducer.java
+++ homework2/project3/src/main/java/com/example/BayesReducer.java
@@ -9,13 +9,10 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;  
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapreduce.Reducer;  
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.hadoop.mapreduce.Reducer; 
 
-public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, IntWritable>{  
-    Logger log = LoggerFactory.getLogger(BayesReducer.class);  
-    private String testFilePath;  
+public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, IntWritable>{ 
+    //private String testFilePath;  
     // 测试数据  
     private ArrayList<int[]> testData = new ArrayList<>();  
     // 保存相同k的所有数据  
@@ -24,16 +21,14 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
     protected void setup(Context context)  
             throws IOException, InterruptedException {  
         Configuration conf = context.getConfiguration();
-        testFilePath = conf.get("/Lsc/homework2/input/application_data2.csv");  
+        //testFilePath = conf.get("/Lsc/homework2/input/application_data2.csv");  
         Path path = new Path("/Lsc/homework2/input/application_data2.csv");  
         FileSystem fs = path.getFileSystem(conf);  
         readTestData(fs,path);  
     }  
     @Override  
-    protected void reduce(IntWritable key, Iterable<MyWritable> values,  
-            Context context)  
-            throws IOException, InterruptedException {  
-        Double[] myTest = new Double[testData.get(0).length-1];  
+    protected void reduce(IntWritable key, Iterable<MyWritable> values,  Context context)throws IOException, InterruptedException {  
+        Double[] myTest = new Double[33];  
         for(int i=0;i<myTest.length;i++){  
             myTest[i] = 1.0;  
         }  
@@ -45,7 +40,7 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
                 myTest[i] += myvalue[i];  
             }  
             sum += 1;  
-        }  
+        }//计算每个类别中属性值为1的P
         for(int i=0;i<myTest.length;i++){  
             myTest[i] = myTest[i]/sum;  
         }  
@@ -70,14 +65,16 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
         int yes = 0;  
         for(int[] test: testData){  
             int value = getClasify(test, labelG);  
-            if(test[0] == value){  
+            if(test[0] == value){
                 yes += 1;  
             }  
             sum +=1;  
             myKey.set(test[0]);  
             myValue.set(value);  
             context.write(myKey, myValue);  
-        }  
+        }
+        System.out.println("测试数据条数为:"+sum);
+        System.out.println("结果正确条数为:"+yes);
         System.out.println("正确率为："+(double)yes/sum);  
     }  
     /*** 
@@ -87,8 +84,7 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
      */  
     private Long getSum(ArrayList<CountAll> allData2) {  
         Long allSum = 0L;  
-        for (CountAll countAll : allData2) {  
-            log.info("类别："+countAll.getK()+"数据："+myString(countAll.getValue())+"总数："+countAll.getSum());  
+        for (CountAll countAll : allData2) { 
             allSum += countAll.getSum();  
         }  
         return allSum;  
@@ -111,16 +107,15 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
                     count += Math.log(pdata[j-1]);   
                 }else{  
                     count += Math.log(1- pdata[j-1]);   
-                }  
-                log.info("count: "+count);  
+                } 
             }  
             count += Math.log(labelG.get(ca.getK()));  
             result[i] = count;  
         }   
         if(result[0] > result[1]){  
-            return 0;  
-        }else{  
             return 1;  
+        }else{  
+            return 0;  
         }  
     }  
     /*** 
@@ -136,14 +131,35 @@ public class BayesReducer extends Reducer<IntWritable, MyWritable, IntWritable, 
         String line = "";  
         while ((line = bf.readLine()) != null) {  
             String[] str = line.split(",");  
-            int[] myData = new int[str.length];  
-            for(int i=0;i<str.length;i++){
-            	if(str[i]!=""||!str[i].equals(""))
-            		if(str[i].matches("^[0-9]+$"))
-                myData[i] = Integer.parseInt(str[i]);  
-            }  
-            testData.add(myData);  
-        }  
+            int[] myData = new int[34];
+            if(str.length>33){
+                boolean skipRow = false; // 用于标记是否跳过当前行
+                for (int i = 0; i < 34; i++) {
+                    if (str[i].isEmpty()) {
+                        skipRow = true;
+                        break;
+                    }
+                    else if(i==0||i>3){
+                        if(!(str[i].matches("^(0|[1-9][0-9]?|100)$"))){
+                            skipRow = true;
+                            break;
+                        }
+                    }
+                }
+                if (!skipRow) {
+                    myData[0] = Integer.parseInt(str[0]);
+                    // 如果为M，则为1；如果为F，则为0
+                    myData[1] = str[1].equalsIgnoreCase("M") ? 1 : 0;
+                    // 如果为Y，则为1；如果为N，则为0
+                    myData[2] = str[2].equalsIgnoreCase("Y") ? 1 : 0;
+                    myData[3] = str[3].equalsIgnoreCase("Y") ? 1 : 0;
+                    for(int i=4; i < 34;i++){
+                        myData[i] = Integer.parseInt(str[i]);
+                    }
+                    testData.add(myData);  
+                }
+            }
+        }
         bf.close();  
         data.close();  
           
